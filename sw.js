efd643b724cbeaf0b1b9a8282dff89272c670b0d
tyamashita-maker <t.yamashita@tsukiha-hosyo.com>
2026-09-26 17:58:58 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* 事業損失 電子白板 — オフライン用 (20260926-174825) */
-var CACHE = "hakuban-20260926-174825";
+/* 事業損失 電子白板 — オフライン用 (20260926-175653) */
+var CACHE = "hakuban-20260926-175653";
 var CORE = ["./","./index.html","./manifest.webmanifest",
             "./icon-180.png","./icon-192.png","./icon-512.png"];
 
